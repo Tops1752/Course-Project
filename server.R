2aaf59f91a986c562ebc31f79357266087ee2f79
Tops1752 <zoubai1989@hotmail.com>
2015-12-22 22:46:03 -0600

    update 2015.12.22

--- server.R
+++ server.R
@@ -1,20 +1,50 @@
+library(ggplot2)
 
 
 
-
-library(UsingR)
-data(mtcars)
 
 
 shinyServer(
     function(input, output) {
-        output$newHist <- renderPlot({
-            hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-            mu <- input$mu
-            lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-            mse <- mean((galton$child - mu)^2)
-            text(63, 150, paste("mu = ", mu))
-            text(63, 140, paste("MSE = ", round(mse, 2)))
-        })
+      
+      output$distPlot <- renderPlot({
+        mu <- input$mn
+        sd <- input$sd
+        
+        distSample <- NULL
+        if(input$dist == "unif"){
+          distSample <- sample(mu,1000,TRUE)
+          dat = data.frame(num = distSample)
+          ggplot(dat,aes(num)) +
+            geom_density(aes(y=..density..)) + 
+            ggtitle("Sample Data")
+          
+        } 
+        else if(input$dist == "norm"){
+          distSample <- rnorm(1000,mu,sd)
+          ggplot(dat,aes(num)) +
+            geom_density(aes(y=..density..)) + 
+            ggtitle("Sample Data")
+          
+        } 
+        else{
+          distSample <- rexp(1000,1/mu)
+          dat = data.frame(num = distSample)
+          ggplot(dat,aes(num)) +
+            geom_density(aes(y=..density..)) + 
+            ggtitle("Sample Data")
+        }
+      })
+      
+      
+      
+#         output$newHist <- renderPlot({
+#             hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
+#             mu <- input$mu
+#             lines(c(mu, mu), c(0, 200),col="red",lwd=5)
+#             mse <- mean((galton$child - mu)^2)
+#             text(63, 150, paste("mu = ", mu))
+#             text(63, 140, paste("MSE = ", round(mse, 2)))
+#         })
     }
 )
--- ui.R
+++ ui.R
@@ -1,25 +1,40 @@
 shinyUI(pageWithSidebar(
    
     
-    headerPanel("Regression Study on mtcars data"),
+    titlePanel("Simple Simulation to learn Central Limit Theorem"),
     
-    sidebarPanel(
-        p("The purpose of this app is to study how the transmission effects the MPG of the motor vehicle. 
-            Particularly, the study focuses on the two questions:"),
-        p( "--Is an automatic or manual transmission better for MPG"),
-        p( "--Quantify the MPG difference between automatic and manual transmissions"),
+    mainPanel(
+        p("In probability theory, the central limit theorem (CLT) states that, 
+          given certain conditions, the arithmetic mean of a sufficiently large 
+          number of iterates of independent random variables, 
+          each with a well-defined expected value and well-defined variance, 
+          will be approximately normally distributed, 
+          regardless of the underlying distribution"),
+        p("This APP builds some simple simulation to help understand CLT"),
         
+        h3("Step 1 - Choose Distribution"),
+        radioButtons("dist", "Distribution Type:",
+                           c("Uniform" = "unif",
+                             "Normal" = "norm",
+                             "Exponential" = "exp")
+                     ),
+        sliderInput("mn", "Input distribution mean:",value = 5, min = 1, max = 10, step = 1,),
+        sliderInput("sd", "Input distribution sd:",value = 2, min = 1, max = 10, step = 2,),
         
-        
-        
+        h3("Step 2 - Set Simulation Parameter"),
+        numericInput('n', 'Sample Size',value = 40, min = 30, max = 80, step = 10,),
+        numericInput('round', 'Times conducting simulation',value = 1000, min = 500, max = 2000, step = 500,),
         
         
         
         sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05,)
     ),
     
-    mainPanel(
-        plotOutput('newHist')
+    
+    sidebarPanel(
+      
+      plotOutput('distPlot')
+      
     )
     
     
