87ee5368e21e8b2eb1e936e73007e62344f17678
Tops1752 <Tops1752@users.noreply.github.com>
2015-12-26 23:02:52 -0600

    2015.12.26

--- ui.R
+++ ui.R
@@ -1,51 +1,50 @@
-shinyUI(pageWithSidebar(
-   
+
+shinyUI(fluidPage(
     
-    titlePanel("Simple Simulation to learn Central Limit Theorem"),
+    titlePanel("Simple Simulation to Learn Central Limit Theorem"),
     
-    mainPanel(
-        p("In probability theory, the central limit theorem (CLT) states that, 
-          given certain conditions, the arithmetic mean of a sufficiently large 
-          number of iterates of independent random variables, 
-          each with a well-defined expected value and well-defined variance, 
-          will be approximately normally distributed, 
-          regardless of the underlying distribution"),
-        p("This APP builds some simple simulation to help understand CLT"),
-        
-        h3("Step 1 - Choose Distribution"),
-        radioButtons("dist", "Distribution Type:",
-                           c("Normal" = "norm",
-                             "Poisson" = "pois",
-                             "Exponential" = "exp")
-                     ),
-        sliderInput("mn", "Input distribution mean:",value = 5, min = 1, max = 10, step = 1,),
-        sliderInput("sd", "Input distribution sd:",value = 1, min = 1, max = 10, step = 2,),
-        
-        h3("Step 2 - Set Simulation Parameter"),
-        p("Choose the sample size and how many times you want to run the simulation."),
-        numericInput('n', 'Sample Size',value = 40, min = 30, max = 80, step = 10,),
-        numericInput('round', 'Times conducting simulation',value = 1000, min = 500, max = 2000, step = 500,),
-        
-        h3("Setp 3 - Run Simulation"),
-        p("Now let's run the simulation to see how does the mean of each sample look like."),
-        actionButton('run','Run Simulation!'),
-        
-        
-        
-        sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05,)
-    ),
+    p("The App runs the simple simulation to help learn about CLT. In specific, how the sample mean value normally 
+    distributed by the theoretical mean. "),
+    p("To start with, choose the distribution you want to test:"),
     
     
-    sidebarPanel(
-      
-      plotOutput('distPlot'),
-      
-      p("Results of simulation:"),
-      plotOutput('meanPlot')
-      
+    sidebarLayout(
+        
+        mainPanel(
+            tabsetPanel(id = "tabset",
+                        tabPanel("Normal",
+                                 sliderInput("mnN", "Input distribution mean:",value = 5, min = 1, max = 10, step = 1,),
+                                 sliderInput("sdN", "Input distribution sd:",value = 1, min = 1, max = 10, step = 2,)
+                        ),
+                        tabPanel("Poisson",
+                                 sliderInput("mnP", "Input distribution mean:",value = 5, min = 1, max = 10, step = 1,)
+                        ),
+                        tabPanel("Exponential",
+                                 sliderInput("mnE", "Input distribution mean:",value = 5, min = 1, max = 10, step = 1,)
+                        )
+            ),
+            
+            h3("Set Simulation Parameter"),
+            p("Choose the sample size and how many times you want to run the simulation."),
+            numericInput('n', 'Sample Size',value = 40, min = 30, max = 80, step = 10,),
+            numericInput('round', 'Times conducting simulation',value = 1000, min = 500, max = 2000, step = 500,),
+            
+            h3("Run Simulation"),
+            p("Now let's run the simulation to see how does the mean of each sample look like."),
+            actionButton("go", "Run Simulation!"),
+            
+            textOutput('Result')
+            
+        ),
+        
+        sidebarPanel(
+            plotOutput('distPlot'),
+            
+            p("Results of simulation:"),
+            plotOutput('meanPlot')
+            
+        )
+        
     )
-    
-    
-    
 )
 )
